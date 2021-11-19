4e05306a88aa8721564d0260f65a83c905161051
Benjamin Marshall <b.m.marshall@stir.ac.uk>
2021-11-19 11:09:05 +0000

    Classified landscape RAM limits dodged by applying classify function to lower res landscape, then disaggregating it to the full 1000x1000. Not ideal, but ok for testing purposes.

--- R/NLMR landscape generation testing.R
+++ R/NLMR landscape generation testing.R
@@ -56,9 +56,9 @@ ggplot() +
 
 ## CLASSIFIED ##
 
-clusterHabs <- NLMR::nlm_gaussianfield(ncol = 1000,
-                                       nrow = 1000,
-                                       autocorr_range = 50,
+clusterHabs <- NLMR::nlm_gaussianfield(ncol = 100,
+                                       nrow = 100,
+                                       autocorr_range = 20,
                                        mag_var = 5,
                                        nug = 0.2,
                                        mean = 0.5,
@@ -67,6 +67,32 @@ clusterHabs <- NLMR::nlm_gaussianfield(ncol = 1000,
 raster::plot(clusterHabs)
 
 ### DONT RUN REQUIRES TOO MUCH RAM?????
-# classHab <- landscapetools::util_classify(clusterHabs, n = 3)
+classHabs <- landscapetools::util_classify(clusterHabs, n = 3)
 
-# raster::plot(classHabs)
+raster::plot(classHabs)
+
+classHabs1000 <- raster::disaggregate(classHabs, fact = 10)
+
+raster::plot(classHabs1000)
+
+classHabsMat <- matrix(data = raster::getValues(classHabs1000),
+                        nrow = 1000,
+                        ncol = 1000)
+
+longClassHabsMat <- reshape2::melt(classHabsMat, c("col", "row"))
+
+ggplot() +
+  geom_raster(data = longClassHabsMat, aes(x = col, y = row, fill = value))
+
+## We do need a solution for people who may only be able to supply low res
+## landscapes, we either need to rescale them (but that may be too memory
+## intensive for large lanscapes) or fix the rounding in the step lengths and
+## how they connect to cell calls. See below (latter options is likely more
+## work, but more flexible, preferable).
+
+# To address a mismatch between raster res and step length we'd need to divide
+# the step lengths by the size of raster cell (e.g., 100 to convert a 100 by 100
+# cell to 1 m by 1 m: ie the same res as the steps) and then floor round to geet
+# the matrix/cell value. This should work, but will need an inout for the res of
+# cells in environmental layers. Would be easier to enforce the layers to all be
+# the same res.
