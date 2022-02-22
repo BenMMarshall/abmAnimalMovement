c72066405471534faf4c6f9ff7ba4789598778d6
Benjamin Marshall <benjaminmichealmarshall@gmai.com>
2022-02-22 10:49:05 +0000

    Cleaned up testing code
    Simple cleaning of test code, proper titles added and reordered.

--- tests/Basic Rcpp Compiling and Testing.R
+++ tests/Basic Rcpp Compiling and Testing.R
@@ -11,85 +11,30 @@ library(patchwork)
 library(scico)
 library(dplyr)
 
+##### SET CONSTANTS ##### ------------------------------------------------------
+
 ### PALETTE ###
 palette <- c("#AD6DED", "#7D26D4", "#E87D13", "#965A1D", "#302010")
 names(palette) <- c("purp1", "purp2", "2", "1", "0")
-
-# Basic Rcpp Compiling and Testing ----------------------------------------
-
+### SEED ###
 set.seed(2022)
 
-# so this works, remember that the index should be one lower cos Cpp starts at zero
-find_max(vect = c(4,6,5))
-find_max(vect = c(4,6,5,10))
-find_max(vect = 0:5)
-find_max(vect = c(4,6,5,4,5,4,32,2,4,4,6,1,7,73,3,2,23,54))
-
-# Sampling sub-function testing -------------------------------------------
-
-# https://www.r-bloggers.com/2018/09/using-rs-set-seed-to-set-seeds-for-use-in-c-c-including-rcpp/
-get_seed <- function() {
-  sample.int(.Machine$integer.max, 1)
-}
-
-get_seed()
-
-sample_options(c(0.4, 0.1, 0.7, 0.1, 0.1, 0.2), get_seed())
-sample_options(c(-0.4, -0.1, -0.7), get_seed())
-sample_options(rep(0, 5), get_seed())
-sample_options(rep(-0.1, 5), get_seed())
-
-sampleOut <- NULL
-for(i in 1:10000){
-  sampleOut[i] <- sample_options(c(2, -0.2, 5, 0.5, 0.05), get_seed())
-}
-hist(sampleOut)
-table(sampleOut) / 10000
-
-# sampleOut <- NULL
-# for(i in 1:10000){
-#   sampleOut[i] <- sample_options(rep(0, 8), get_seed())
-# }
-# hist(sampleOut)
-
-# Dist to des normalise ---------------------------------------------------
-
-# distsTests <- c(50,20,324,234,683,29,101)
-distsTests <- rgamma(12, 5, 0.5)
-
-distInvert = abs(distsTests - max(distsTests))
-weights_toDes = (distInvert - min(distsTests)) /
-  (max(distsTests) - min(distsTests))
-
-weights_toDes^2
-
-# Matrix BG creation ------------------------------------------------------
+##### Generate environmental rasters ##### -------------------------------------
 
 envNoiseTest <- genLandscape_noise(2000, 2000)
 # quick_plot_matrix(envNoiseTest)
-#
+
 # envGradMat <- genLandscape_gradient(1000, 1000)
 # quick_plot_matrix(envGradMat)
 
 landcapeLayersList <- genLandscape_quickTriple(2000, 2000, seed = 1)
 
-# weaken the impact of the movement layer
-# landcapeLayersList$shelter[] <- landcapeLayersList$shelter[]/2
-# remove the impact for testing
-# landcapeLayersList$shelter[] <- 0
-
+# Select which environment for post-simulation mapping
 # plotBgEnv <- quick_plot_matrix(landcapeLayersList$memShelter)
 # plotBgEnv <- quick_plot_matrix(landcapeLayersList$shelter)
 plotBgEnv <- quick_plot_matrix(landcapeLayersList$forage)
 
-# plotBgEnv
-
-# Select envMat to use ----------------------------------------------------
-
-# envMatTest <- envNoiseTest
-# plotBgEnv <- quick_plot_matrix(envMatTest)
-
-# Generate transitional matrix  --------------------------------------------
+##### Create behavioural transitional matrix ###### ----------------------------
 
 b0 <- c(0.98, 0.001, 0.00001) # rest
 b1 <- c(0.0005, 0.99, 0.02) # explore/move
@@ -104,7 +49,7 @@ shelterLocs <- data.frame(
   "y" = c(1020, 1050, 1050, 1010, 1045)
 )
 
-# Random walk testing -----------------------------------------------------
+##### Run core simulation function ##### ---------------------------------------
 
 simRes <- abm_simulate(start = c(1050,1050),
                        steps = 24*60 *7,
@@ -124,8 +69,9 @@ simRes <- abm_simulate(start = c(1050,1050),
                        move_Options = landcapeLayersList$shelter) # just using a place holder layer for testing
 
 
-# Review step lengths -----------------------------------------------------
+##### Review step lengths and turn angles ##### --------------------------------
 
+### STEP LENGTHS ###
 stepData <- simRes$locations %>%
   mutate(sl = sqrt(
     (x - lag(x))^2 +
@@ -177,6 +123,7 @@ stepData %>%
 ggsave("./output/figures/stepBehaviours.png",
        width = 180, height = 160, units = "mm", dpi = 300)
 
+### TURN ANGLE ###
 for(beh in 0:2){
 
   taPlot <- simRes$locations %>%
@@ -216,14 +163,13 @@ for(beh in 0:2){
 
 }
 
-## have to loop and combine to fix y axis issues
+# have to loop and combine to fix y axis issues
 taPlot_0 / taPlot_1 / taPlot_2
 
 ggsave("./output/figures/turnangleBehaviours.png",
        width = 180, height = 160, units = "mm", dpi = 300)
 
-
-# Mapped output -----------------------------------------------------------
+##### Mapping movements ##### --------------------------------------------------
 
 plotBgEnv +
   geom_point(data = simRes$options,
@@ -264,10 +210,10 @@ plotBgEnv +
 ggsave("./output/figures/overallMapping.png",
        width = 180, height = 180, units = "mm", dpi = 300)
 
-# Behaviour state switching check -----------------------------------------
+##### Behaviour state cycling and switching ##### ------------------------------
 
-# rest_Cycle = c(0.65, -0.3, 24, 24),
-# 24*60 *7
+### CYCLING ###
+# 24*60 *7 # weeks worth of data
 behaveProb <- sapply(1:10080/60, function(x){
   cycle_draw(x, 0.65, -0.3, 24, 24)
 })
@@ -335,6 +281,7 @@ plotObsBehave / plotExpRest + plot_layout(heights = c(1,0.5))
 ggsave("./output/figures/behaviourCycle.png",
        width = 180, height = 120, units = "mm", dpi = 300)
 
+### BEHAVE TRANSITIONS ###
 simRes$locations$behave
 behaveTrans <- list("vector", length(simRes$locations$behave))
 behaveTransDF <- data.frame("behaveS" = rep(NA, length(simRes$locations$behave)),
@@ -401,14 +348,99 @@ rbind(observedBehaveChanges, expectedBehaveChanges) %>%
 ggsave("./output/figures/behaviourTransitions.png",
        width = 180, height = 180, units = "mm", dpi = 300)
 
-# Animate movement --------------------------------------------------------
+##### Sampling function testing ##### ------------------------------------------
+
+# https://www.r-bloggers.com/2018/09/using-rs-set-seed-to-set-seeds-for-use-in-c-c-including-rcpp/
+get_seed <- function() {
+  sample.int(.Machine$integer.max, 1)
+}
+
+ndraws <- 10000
+
+sampleOut <- NULL
+for(i in 1:ndraws){
+  sampleOut[i] <- sample_options(c(2, -0.2, 5, 0.5, 0.05), get_seed())
+}
+# hist(sampleOut)
+# table(sampleOut) / ndraws
+
+data.frame(
+  "value" = c("2", "5", "0.5", "0.05", "-0.2"),
+  "result" = c(table(sampleOut), 0)) %>%
+  ggplot() +
+  geom_col(aes(x = reorder(value, -result), y = result)) +
+
+  theme_bw() +
+  theme(legend.position = "none",
+        axis.title = element_text(angle = 0,
+                                  face = 2,
+                                  hjust = 0.5),
+        axis.title.y = element_text(angle = 0,
+                                    face = 2,
+                                    hjust = 1),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 12, face = 4,
+                                  hjust = 0),
+        axis.line = element_line(size = 0.5)) +
+  labs(x = "Probability", y = paste0("Count\n(of ", ndraws, ")"))
+
+ggsave("./output/figures/sampleOptions.png",
+       width = 160, height = 160, units = "mm", dpi = 300)
+
+##### Von-Mises draw testing ##### ---------------------------------------------
+
+citation("CircStats")
+# mu should vary between 0 and 2*pi for circular stuff
+
+vonOut <- vonmises(N = 1000, MU = 0, KAPPA = 0.1)
+hist(vonOut)
+
+vonResVarying <- do.call(rbind, lapply(
+  c(0.01, seq(0.05, 0.8, 0.1)),
+  function(k){
+    print(k)
+    return(data.frame(kappa = paste("KAPPA =",
+                                    format(k, nsmall = 2)), # format forces the zeroes to remain
+                      draws = vonmises(N = 2000, MU = 0, KAPPA = k)))
+  }))
+
+sum(vonResVarying$draws > pi)
+sum(vonResVarying$draws < -pi)
+
+# \u03c0 is unicode for pi
+## display the varying levels of angle concentration.
+ggplot() +
+  geom_histogram(data = vonResVarying, aes(x = draws),
+                 fill = "black", alpha = 0.5,
+                 binwidth = 0.25) +
+  scale_x_continuous(breaks = c(-pi, -pi/2, 0, pi/2, pi),
+                     labels = c("-\u03c0", "-\u03c0/2", "0", "\u03c0/2", "\u03c0"),
+                     limits = c(-pi, pi), # implementing pi limits cuts of a few points, might be an approximation issue.
+                     # might be a source of issues in the random walk, might need a redraw if those limits are exceeded. Not sure
+                     # if it'd break something in the walk. Could implement check for safety.
+                     # Seems to be an approximation issue with ggplot, as checking for values >pi | <-pi return zero results
+                     expand = c(0,0)
+  ) +
+  coord_polar() +
+  theme_void() +
+  theme(axis.text.x = element_text(vjust = 1),
+        strip.text = element_text(face = 4, hjust = 0.5),
+        # panel.grid.major.x = element_line(colour = "grey15"),
+        plot.background = element_rect(colour = NA, fill = "white"),
+        panel.background = element_rect(colour = NA, fill = "white")) +
+  facet_wrap(.~kappa)
+
+ggsave("./output/figures/vonmisesKappa.png",
+       width = 180, height = 180, units = "mm", dpi = 300)
+
+##### Animate movement ##### ---------------------------------------------------
 
 # install.packages("gifski")
 # install.packages("av")
 library(gganimate)
 library(dplyr)
-
-?`gganimate-package`
 
 locations <- data.frame(
   step = simRes$loc_step,
@@ -468,110 +500,3 @@ anim_save(path = "./output/figures/",
           file = "animated_locations.mp4",
           animation = last_animation())
 
-
-# Vonmises testing --------------------------------------------------------
-
-library(abmAnimalMovement)
-library(ggplot2)
-
-set.seed(2021)
-
-vonOut <- vonmises(N = 1000, MU = 0, KAPPA = 0.1)
-hist(vonOut)
-
-vonResVarying <- do.call(rbind, lapply(
-  c(0.01, seq(0.05, 0.8, 0.1)),
-  function(k){
-    print(k)
-    return(data.frame(kappa = paste("KAPPA =",
-                                    format(k, nsmall = 2)), # format forces the zeroes to remain
-                      draws = vonmises(N = 2000, MU = 0, KAPPA = k)))
-  }))
-
-sum(vonResVarying$draws > pi)
-sum(vonResVarying$draws < -pi)
-citation("CircStats")
-# mu should vary between 0 and 2*pi for circular stuff
-# https://www.zeileis.org/news/circtree/
-
-# soltuion now adapted to C++
-# CircStats::rvm()
-
-# \u03c0 is unicode for pi
-## display the varying levels of angle concentration.
-ggplot() +
-  geom_histogram(data = vonResVarying, aes(x = draws),
-                 fill = "black", alpha = 0.5,
-                 binwidth = 0.25) +
-  scale_x_continuous(breaks = c(-pi, -pi/2, 0, pi/2, pi),
-                     labels = c("-\u03c0", "-\u03c0/2", "0", "\u03c0/2", "\u03c0"),
-                     limits = c(-pi, pi), # implementing pi limits cuts of a few points, might be an approximation issue.
-                     # might be a source of issues in the random walk, might need a redraw if those limits are exceeded. Not sure
-                     # if it'd break something in the walk. Could implement check for safety.
-                     # Seems to be an approximation issue with ggplot, as checking for values >pi | <-pi return zero results
-                     expand = c(0,0)
-  ) +
-  coord_polar() +
-  theme_void() +
-  theme(axis.text.x = element_text(vjust = 1),
-        strip.text = element_text(face = 4, hjust = 0.5),
-        # panel.grid.major.x = element_line(colour = "grey15"),
-        plot.background = element_rect(colour = NA, fill = "white"),
-        panel.background = element_rect(colour = NA, fill = "white")) +
-  facet_wrap(.~kappa)
-
-ggsave("./output/figures/vonmisesKappa.png",
-       width = 180, height = 180, units = "mm", dpi = 300)
-
-simRes$locations$sl
-simRes$locations$ta
-round(simRes$locations$ta, digits = 2)
-round(simRes$locations$ta * 180/pi, digits = 2)
-simRes$locations %>%
-  ggplot() +
-  geom_histogram(aes(x = ta)) +
-  facet_wrap(.~behave)
-
-# Testing matrix conversion and back --------------------------------------
-
-row <- 1000; col <- 1000
-# random matrix
-envMatTest <- matrix(runif(row*col, 0, 1), nrow = row, ncol = col)
-# split matrix, animal should remain more often in one side
-# chaning something so we can spot if mistakes are made
-envMatTest[1:250, 1:250] <- 1
-
-
-colnames(envMatTest) <- 1:col
-rownames(envMatTest) <- 1:row
-
-matrix_combine(envMatTest)
-
-
-# Using Rcpp to call and compile Cpp function directly --------------------
-
-####
-### will only work if // [[Rcpp::export]] present ###
-####
-Rcpp::sourceCpp("./src/walk_options_xy.cpp")
-
-walkRes <- walk_options_xy(startx = 20,
-                           starty = 25,
-                           steps = 200,
-                           options = 12,
-                           normmean = 5,
-                           normsd = 5,
-                           meanang = 0,
-                           sdang = 180)
-
-plot(walkRes[,1], walkRes[,2])
-lines(walkRes[,1], walkRes[,2])
-
-# possible benchmarking method -----------------------------------------------------
-
-# install.packages("microbenchmark")
-
-microbenchmark::microbenchmark(
-  mat[100,230],
-  getValueFromRaster(mat, 99, 229)
-)
