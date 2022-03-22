2b34b5368a4a372207f32fc587a94834b3537869
Benjamin Marshall <b.m.marshall@stir.ac.uk>
2022-03-22 11:21:06 +0000

    Starting adding details of the three species
    Thinking badger, vulture and cobra are the way to go. Badger can have avoidance added, and cobra can have a hard barrier?

--- tests/TO DO LIST.R
+++ tests/TO DO LIST.R
@@ -4,15 +4,10 @@
 ## Add custom input for size of shelter site (ie distance where rest behaviour
 # becomes near stationary)
 
+## Add system for points of avoidance, i.e. shelter site attraction in reverse
+
 ## Expand range of rasters
 
 ## Ensure that each raster is connected to correct behaviour. Movement vs
 # decisions
 
-## FIX angle, as it is currently aiming always for North.
-# Need double to store last chosen angle
-# Need a vector length nopt to store angle options movement
-# double of last angle will be used in calculating next destination and next movement:
-# line 283, 342
-# think we just need to add the new drawn angles to a previous angle
-# make sure to update currAngle each time
