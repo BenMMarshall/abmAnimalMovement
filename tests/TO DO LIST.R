af13e671160e64127f428dc7a0c6bfd3cc82fe42
Benjamin Marshall <b.m.marshall@stir.ac.uk>
2022-03-22 16:06:54 +0000

    Intro and justification for the model worked on
    Intro has been a little more fleshed out, the general plan is there if the latter justification compared to other simulation efforts is weak right now. Needs cites, and supporting examples throughout.

--- tests/TO DO LIST.R
+++ tests/TO DO LIST.R
@@ -6,6 +6,8 @@
 
 ## Add system for points of avoidance, i.e. shelter site attraction in reverse
 
+## Add a system for a second cycling, e.g. hibernation or snake digestion cycle
+
 ## Expand range of rasters
 
 ## Ensure that each raster is connected to correct behaviour. Movement vs
