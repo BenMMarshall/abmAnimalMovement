1ca1f441017f02e15adc895c2d8bfd9250992e9b
Benjamin Marshall <benjaminmichealmarshall@gmai.com>
2022-06-06 15:20:30 +0100

    New abstract and fixed figures
    Now knits on linux without issues, file size dramatically reduced by rendering figures as pngs. All figures have captions, in a decent first draft state although extras like data statement and acknowledgements are incomplete.

--- notebook/vignettes_manuscript/Render and move code.R
+++ notebook/vignettes_manuscript/Render and move code.R
@@ -4,9 +4,9 @@ rmarkdown::render(input = here::here("notebook",
                                "Agent-based_model_walkthrough.Rmd"),
                   output_format = "bookdown::pdf_document2")
 
-file.copy(from = here("notebook",
+file.copy(from = here::here("notebook",
                       "vignettes_manuscript",
                       "Agent-based_model_walkthrough.pdf"),
-          to = here("Vignettes", "Agent-based_model_walkthrough.pdf"),
+          to = here::here("vignettes", "Agent-based_model_walkthrough.pdf"),
           overwrite = TRUE)
 # file.remove(here("Agent-based_model_walkthrough.pdf"))
